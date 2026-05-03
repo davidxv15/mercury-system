3f39fd5f51311244ae8e2c8c721ba66bfa5c99da
davidxv15 <davidxvaz@gmail.com>
2026-05-03 09:26:29 -0700

    feat(products.ts) product objects

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,11 +3,29 @@ import type { Product } from "../types/Product";
 export const products: Product[] = [
   {
     id: "p001",
+    name: "",
+    category: "",
+    price: ,
+    stock: ,
+    imageURL: "",
+    featured: ,
   },
   {
     id: "p002",
+    name: "",
+    category: "",
+    price: ,
+    stock: ,
+    imageURL: "",
+    featured: ,
   },
   {
     id: "p003",
+    name: "",
+    category: "",
+    price: ,
+    stock: ,
+    imageURL: "",
+    featured: ,
   },
 ];
