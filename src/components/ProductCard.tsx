59b64c52dc32e0d887547c5e9b1a32a78f9e8092
davidxv15 <davidxvaz@gmail.com>
2026-05-03 11:14:41 -0700

    feat(ProductCard.tsx) stub function

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import type { Product } from "../types/Product";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    
+  )
+}
