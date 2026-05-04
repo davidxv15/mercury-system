9d64892d59bde15c884046c1a12d555770301cf7
davidxv15 <davidxvaz@gmail.com>
2026-05-03 20:57:24 -0700

    feat(Shop.tsx) product card

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,9 +1,21 @@
+import ProductCard from "../components/ProductCard";
+import { products } from "../data/products";
+
 function Shop() {
   return (
     <div className="p-4">
       <h2 className="text-3xl font-semibold text-stone-200">
         Shop
       </h2>
+
+      <section className="grid gap-4 md:grid-col-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product} />
+        ))}
+    </section>
+
     </div>
   );
 }
