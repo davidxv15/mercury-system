b144ebc73a901b77e13516d245e6b8ef2b6c60df
davidxv15 <davidxvaz@gmail.com>
2026-05-05 09:52:34 -0700

    feat(MainLayout) import useCart, cartCount

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { use } from "react";
 
 function MainLayout() {
+  const { cartCount } = useCart();
   return (
     <div className="min-h-screen bg-zinc-900 text-amber-600">
       <nav className="flex items-center gap-6 border-b border-zinc-700 px-6 py-4">
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">Cart ({cartCount})</Link>
         <Link to="/admin">Admin</Link>
       </nav>
 
