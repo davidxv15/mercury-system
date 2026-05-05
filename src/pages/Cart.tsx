831abfd7f5ec168726d17617ab2a55df73e9de2d
davidxv15 <davidxvaz@gmail.com>
2026-05-05 12:27:06 -0700

    feat(Cart.tsx) cart display logic

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,41 @@
+import { useCart } from "../context/CartContext";
+
 function Cart() {
+  const { cartItems, cartCount } = useCart();
+
   return (
     <div className="p-4">
-      <h2 className="text-3xl font-semibold text-stone-200">
+      <h2 className="mb-6 text-3xl font-semibold text-stone-200">
         Cart
       </h2>
+
+      {cartItems.length === 0 ? (
+        <p className="text-stone-400">
+          Your cart is empty.
+        </p>
+      ) : (
+          <section>
+            <p className="text-stone-400">Total items: {cartCount}</p>
+
+            {cartItems.map((item) => (
+              <article
+                key={item.product.id}
+                className="rounded-xl border border-zinc-700 bg-zinc-800 p-4">
+                <h3 className="text-xl font-semibold text-stone-100">
+                  {item.product.name}
+                </h3>
+
+                <p className="text-stone-400">
+                  Quantity: {item.quantity}
+                </p>
+                <p className="font-bold text-amber-400">
+                  ${item.product.price * item.quantity}
+                </p>
+              </article>
+            ))}
+        </section>
+      )}
+
     </div>
   );
 }
