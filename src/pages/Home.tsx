9f7f3bd511d54d9ef43dc5ed561be5e72d2b7dc0
davidxv15 <davidxvaz@gmail.com>
2026-05-07 20:29:42 -0700

    feat(Home.tsx) article2: styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ function Home() {
           </p>
         </article>
 
-        <article className="">
+        <article className="rounded-2xl border border-zinc-700 bg-zinc-800 p-5">
           <h2 className="text-xl">Admin</h2>
           <p className="mt-2">
             Inventory, orders, metrics, and business operations tooling.
