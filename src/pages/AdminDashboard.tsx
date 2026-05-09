c0ad9529533b328cd284fef33537b2dc21fd782b
davidxv15 <davidxvaz@gmail.com>
2026-05-08 22:56:51 -0700

    feat(AdminDashboard.tsx) const totalStock

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -2,6 +2,8 @@ import { products } from "../data/products";
 
 function AdminDashboard() {
   const totalProducts = products.length;
+  const totalStock = products.reduce((total, product) => total + product.stock, 0);
+  
   return (
     <div className="p-4">
       <h2 className="text-3xl font-semibold text-stone-200">
