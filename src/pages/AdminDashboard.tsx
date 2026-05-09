360dd752b6afaf91b65d937b72cfc09be0425857
davidxv15 <davidxvaz@gmail.com>
2026-05-08 22:58:10 -0700

    feat(AdminDashboard.tsx) const featuredProducts

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -3,7 +3,8 @@ import { products } from "../data/products";
 function AdminDashboard() {
   const totalProducts = products.length;
   const totalStock = products.reduce((total, product) => total + product.stock, 0);
-  
+  const featuredProducts = products.filter((product) => product.featured).length;
+
   return (
     <div className="p-4">
       <h2 className="text-3xl font-semibold text-stone-200">
