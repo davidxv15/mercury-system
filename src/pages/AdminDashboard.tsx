bb35521dab3ecec1d020dc74e5ae457e62ead7f2
davidxv15 <davidxvaz@gmail.com>
2026-05-11 22:45:19 -0700

    feat(AdminDashboard.tsx) article2

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -16,6 +16,10 @@ function AdminDashboard() {
           <p className="text-sm text-stone-400">Products</p>
           <h3 className="text-3xl font-bold text-stone-100">{totalProducts}</h3>
         </article>
+        <article className="rounded-2xl">
+          <p className="text-sm text-stone-400">Total Stock</p>
+          <h3 className="text-3xl font-bold text-stone-100"></h3>
+        </article>
       </section>
 
     </div>
