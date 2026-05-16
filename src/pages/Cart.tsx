fce8ccde3c84fd9dda8434ecbca035ccdebf86af
davidxv15 <davidxvaz@gmail.com>
2026-05-15 21:47:46 -0700

    style(Cart.tsx) p-6

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -16,7 +16,7 @@ function Cart() {
       </h2>
 
       {cartItems.length === 0 ? (
-        <p className="rounded-2xl border border-zinz-700 bg-zinc-800 text-stone-400">
+        <p className="rounded-2xl border border-zinz-700 bg-zinc-800 text-stone-400 p-6">
           Your cart is empty.
         </p>
       ) : (
