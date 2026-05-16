11eb1ace983157345dee66d9a7f473976734714e
davidxv15 <davidxvaz@gmail.com>
2026-05-15 21:40:59 -0700

    feat(Cart.tsx) const orderTotal

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,6 +4,11 @@ import { useCart } from "../context/CartContext";
 function Cart() {
   const { cartItems, cartCount } = useCart();
 
+  const orderTotal = cartItems.reduce(
+    (total, item) => + item.product.price * item.quantity,
+    0
+  );
+  
   return (
     <div className="p-4">
       <h2 className="mb-6 text-3xl font-semibold text-stone-200">
