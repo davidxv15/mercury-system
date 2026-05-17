57908081b4c8bdd623e638fc6e35b2af0e42f1e4
davidxv15 <davidxvaz@gmail.com>
2026-05-16 18:51:54 -0700

    feat(App.tsx) Route productId

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         <Route element={<MainLayout />} >
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="ProductId" element={productId} />
         <Route path="/cart" element={<Cart />} />
           <Route path="/admin" element={<AdminDashboard />} />
           </Route>
