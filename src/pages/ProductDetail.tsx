53921e970b8f5c4e0fb922ab0f26588e3ef534a2
davidxv15 <davidxvaz@gmail.com>
2026-05-16 19:03:48 -0700

    feat(ProductDetail.tsx) function ProductDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,3 +1,12 @@
 import { Link, useParams } from "react-router-dom"
 import { products } from "../data/products"
 import { useCart } from "../context/CartContext"
+
+function ProductDetail() {
+  const { productId } = useParams();
+  const { addToCart } = useCart();
+
+  const product = products.find((item) => item.id === productId);
+
+  
+}
