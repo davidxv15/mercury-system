374a5489180c1303c7b2a357e791ea0b1d5c6f69
davidxv15 <davidxvaz@gmail.com>
2026-05-16 18:53:42 -0700

    feat(ProductDetail.tsx) stub

---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,11 @@
+function ProductId() {
+  return (
+    <div className="p-4">
+      <h3 className="text-amber-400">
+        Prodcut Id
+      </h3>
+    </div>
+  );
+}
+
+export default ProductId;
