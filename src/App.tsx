452d9c1d2dd7c43b98829c6560b0bbf7abaa3dfa
davidxv15 <davidxvaz@gmail.com>
2026-05-19 21:18:42 -0700

    feat(ProductDetail.tsx) import ProductDetail

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ProductId from "./pages/ProductDetail";
+import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import AdminDashboard from "./pages/AdminDashboard";
 
