1a60374c79eca7148b12abf93cc208bc6690e093
davidxv15 <davidxvaz@gmail.com>
2026-05-19 21:40:54 -0700

    feat(ProductFilters.tsx) stub

---
+++ src/components/ProductFilters.tsx
@@ -0,0 +1,11 @@
+function ProductFilters() {
+  return (
+    <div className="p-4">
+      <h2 className="text-amber-400 text-2xl">
+        filter component
+      </h2>
+    </div>
+  );
+}
+
+export default ProductFilters;
