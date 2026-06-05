c47de8b65f7e1d37c57c7261d91368c4e9f8fd9d
davidxv15 <davidxvaz@gmail.com>
2026-06-04 17:30:20 -0700

    file move - mustangs.ts to data/vehicles

---
+++ src/data/vehicles/mustangs.ts
@@ -0,0 +1,206 @@
+import { Mustang } from "../../types/Mustang";
+
+export const mustangs: Mustang[] = [
+
+  // ===============================
+  //  2025 ECOBOOST
+  // ===============================
+  
+  {
+    id: "",
+    year: 0,
+    trim: "",
+    bodyStyle: "Fastback",
+
+    engine: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    liters: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    cylinders: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    horsepower: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    torque: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    transmission: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    drivetrain: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+    
+    keyFeatures: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+  // ===================================
+  // 2025 GT
+  // ===================================
+
+  {
+    id: "",
+    year: 0,
+    trim: "",
+    bodyStyle: "",
+
+    engine: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    liters: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+      
+    cylinders: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+    
+    horsepower: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    torque: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    transmission: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    drivetrain: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    keyFeatures: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+  // ==========================
+  // 2025 Dark Horse
+  // ==========================
+  
+  {
+    id: "",
+    year: 0,
+    trim: "",
+    bodyStyle: "",
+
+    engine: {
+      value: "",
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    liters: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    cylinders: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    horsepower: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    torque: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    transmission: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    drivetrain: {
+      value: 0,
+      definition: "",
+      analogy: "",
+      buyerValue: ""
+    },
+
+    keyFeatures: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  }
+
+];
