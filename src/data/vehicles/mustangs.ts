c2eb70253e92d20f359684f77342a83a36c2d5ef
davidxv15 <davidxvaz@gmail.com>
2026-07-01 23:01:46 -0700

    feat(mustangs.ts) 2025 GT : horsepower: analogy

--- src/data/vehicles/mustangs.ts
+++ src/data/vehicles/mustangs.ts
@@ -103,7 +103,7 @@ export const mustangs: Mustang[] = [
     horsepower: {
       value: 480,
       definition: "Engine output.",
-      analogy: "",
+      analogy: "Top-end speed potential.",
       buyerValue: ""
     },
 
