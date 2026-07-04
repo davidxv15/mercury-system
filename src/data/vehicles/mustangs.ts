cefb4183280e7915780fdfbfd8a7f1c351c4b0aa
davidxv15 <davidxvaz@gmail.com>
2026-07-03 22:04:56 -0700

    feat(mustangs.ts) 2025 darkhorse engine : analogy

--- src/data/vehicles/mustangs.ts
+++ src/data/vehicles/mustangs.ts
@@ -149,7 +149,7 @@ export const mustangs: Mustang[] = [
     engine: {
       value: "5.0 V8 (High Performance)",
       definition: "Upgraded V8 tuned for track performance.",
-      analogy: "",
+      analogy: "Race-prepped version of the same engine.",
       buyerValue: ""
     },
 
