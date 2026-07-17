531b3e979c55488336f1032600719575c8419b08
davidxv15 <davidxvaz@gmail.com>
2026-07-16 20:59:49 -0700

    feat(broncos.ts) 2026 bronco base : torque: buyer

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -28,7 +28,8 @@ export const broncos: Vehicle[] = [
     torque: {
       value: "TBD",
       definition: "Rotational shove from the engine.",
-      analogy: "The push into your seat."
+      analogy: "The push into your seat.",
+      buyerValue: ""
     }
   }
 ];
