12bd0db58408695e9ef13d620380466f9dc6e8b2
davidxv15 <davidxvaz@gmail.com>
2026-07-22 23:46:48 -0700

    feat(broncos.ts) 2026 bronco big bend: engine: analogy

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -65,7 +65,7 @@ export const broncos: Vehicle[] = [
     engine: {
       value: "2.3 Ecoboost I-4",
       definition: "Turbocharged four-cylinder engine.",
-      analogy: "",
+      analogy: "Efficient trail runner.",
       buyerValue: ""
     }
 
