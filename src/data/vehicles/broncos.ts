e1357540b973dd089deba952c18c10d180143054
davidxv15 <davidxvaz@gmail.com>
2026-08-29 16:58:19 -0700

    feat(broncos.ts) 2026 bronco big bend: torque: value

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -80,7 +80,7 @@ export const broncos: Vehicle[] = [
       value: "TBD",
       definition: "Twisting force.",
       analogy: "Low-speed muscle.",
-      buyerValue; ""
+      buyerValue; "Useful for trails and takeoff feel."
     }
 
   }
