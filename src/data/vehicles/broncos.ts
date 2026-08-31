5ad5f103bc1952008b2635ba386e03acd41d3c4b
davidxv15 <davidxvaz@gmail.com>
2026-08-30 22:53:28 -0700

    feat(broncos.ts) 2026 bronco big bend: drivetrain: def

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -85,7 +85,7 @@ export const broncos: Vehicle[] = [
 
     drivetrain: {
       value: "4x4",
-      definition: "",
+      definition: "Four-wheel-drive capability.",
       analogy: "",
       
     }
