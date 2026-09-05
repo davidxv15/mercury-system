a17ae0515f94ec199ee67af1b7b0718f4479fab6
davidxv15 <davidxvaz@gmail.com>
2026-09-04 23:49:44 -0700

    feat(broncos.ts) 2026 bronco outer banks: engine: buyer value

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -117,8 +117,8 @@ export const broncos: Vehicle[] = [
       value: "2.3 Ecoboost I-4 / available 2.7 Ecoboost V6",
       definition: "Standard turbo four-cylinder or available stronger V6.",
       analogy: "Regular athelete or bigger athelete.",
-      buyerValue: ""
-    }
+      buyerValue: "More comfort trim with optimal stronger power."
+    },
   }
 
 ];
