0f0a13a8d4c8c92ca6823e9823540ce9e92f5a0f
davidxv15 <davidxvaz@gmail.com>
2026-09-18 21:36:26 -0700

    feat(broncos.ts) 2026 bronco-heritage: engine: def #

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -211,7 +211,7 @@ export const broncos: Vehicle[] = [
 
     engine: {
       value: "2.3 Ecoboost I-4 / available 2.7L Ecoboost V6",
-      definition: "",
+      definition: "Modern turbo engine choices with retro styling.",
       analogy: "",
       buyerValue: ""
     },
