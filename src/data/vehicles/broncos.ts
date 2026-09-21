ddad0deae82089a72f14404350dda9659db1e248
davidxv15 <davidxvaz@gmail.com>
2026-09-20 23:55:56 -0700

    feat(broncos.ts) 2026 bronco heritage: torque: analogy

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -230,7 +230,7 @@ export const broncos: Vehicle[] = [
     torque: {
       value: "TBD",
       definition: "Twisting force.",
-      analogy: "",
+      analogy: "Pulling/climbing shove.",
       buyerValue: ""
     }
 
