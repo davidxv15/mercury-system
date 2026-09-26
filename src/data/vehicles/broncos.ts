b9ef614ed1e8cf7e22c4eaecf7de1c2e1a082e1b
davidxv15 <davidxvaz@gmail.com>
2026-09-25 20:02:46 -0700

    feat(broncos.ts) 2026 bronco stroppe: drivetrain: def #

--- src/data/vehicles/broncos.ts
+++ src/data/vehicles/broncos.ts
@@ -285,7 +285,7 @@ export const broncos: Vehicle[] = [
 
     drivetrain: {
       value: "4x4",
-      definition: "",
+      definition: "Four-wheel-drive.",
       analogy: "",
       buyerValue: ""
     }
